3ce18a4d96661929b8e19955868c3c245205ae3d
Mohammad Farseen <farseenmanekhan1232@gmail.com>
2023-06-03 12:11:10 +0530

    adding files

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+  const position = document.documentElement.scrollTop;
+  console.log(position);
+  if (position >= 400) {
+    document.querySelector(".circ").style.visibility = "hidden";
+  } else {
+    document.querySelector(".circ").style.visibility = "visible";
+  }
+});
